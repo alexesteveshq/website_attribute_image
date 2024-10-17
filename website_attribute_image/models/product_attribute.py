cd61b119cf6095f45b5d3525d3ef9f85aac0228a
alex <alexesteveshq@gmail.com>
2024-10-17 20:41:14 +0200

    [Alex] fix image on backend

--- website_attribute_image/models/product_attribute.py
+++ website_attribute_image/models/product_attribute.py
@@ -6,8 +6,9 @@ from odoo import fields, models
 class ProductAttributeValue(models.Model):
     _inherit = "product.attribute.value"
 
-    website_product_detail_image = fields.Binary(
-        string="Website detail image",
-        attachment=True,
-        help="Image of the attribute value for shop online product detail.",
+    image = fields.Image(
+        string="Image",
+        help="You can upload an image that will be used as the color of the attribute value.",
+        max_width=False,
+        max_height=False,
     )
