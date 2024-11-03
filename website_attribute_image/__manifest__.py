746de217c7ffbab8100a0a17c11485f4bd3f8fc6
alex <alexesteveshq@gmail.com>
2024-11-03 15:15:18 +0100

    [Alex] add website

--- website_attribute_image/__manifest__.py
+++ website_attribute_image/__manifest__.py
@@ -8,6 +8,7 @@
     'version': '18.0.1.0',
     'category': 'Website',
     'author': 'Visionee',
+    'website': 'https://visionee.net',
     'license': 'OPL-1',
     'depends': [
         'website_sale', 'stock',
