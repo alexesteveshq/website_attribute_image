0463b475b82131979e355e7958af86b4d4bb589e
alex <alexesteveshq@gmail.com>
2025-01-10 22:48:29 +0100

    [Alex] fix images feature

--- website_attribute_image/__manifest__.py
+++ website_attribute_image/__manifest__.py
@@ -8,6 +8,7 @@
     'version': '15.0.1.0',
     'category': 'Website',
     'author': 'Visionee',
+    'website': 'https://visionee.net',
     'license': 'OPL-1',
     'depends': [
         'website_sale', 'stock',
